dc705046e5de06f0fd7ecbee26bcfd9cc0271b1d
shurjeel786 <shurjeelk2@gmail.com>
2021-12-15 20:59:19 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,64 @@
+// var firstName = prompt("Enter First Name")
+// var LastName = prompt("Last First Name")
+// var fullName = firstName = LastName
+// console.log(fullName)
+
+
+// document.write("My favorite phone is: Samsung Galaxy S6 Edge Plus "<br>" lenght of string: 28")
+
+
+// document.write("String Pakistani "<br>" Index of 'n': 7")
+
+// document.write("String Hello World "<br>" last Index of '1' : 9")
+
+// document.write("String Pakistani "<br>" Character at Index 3 : i")
+
+// var str = "Hyderabad";
+// console.log(str.replace('hyder','islam'));
+
+// var str = "And";
+// console.log(str.replace('and','&'));
+
+// var mobile =prompt("enter your favourite mob name")
+// var charsImobile = mobile length
+// console.log(charsImobile)
+
+
+// var arr =("pakistani")
+// console.log(arr[7]);
+
+
+// var arr = ("hellow World")
+// console.log(arr[3])
+
+// var arr = ("Pakistani")
+// console.log(Arr)
+
+// var currentAge = prompt("Enter your age.");
+// var yearsEligibleToVote = currentAge - 18;
+
+// var currentAge = prompt("Enter your age.");
+// var qualifying Age = currentAge + 1;
+ 
+// var a = "peanut";
+// var b = a.toUpperCase();
+// alert(b);
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
